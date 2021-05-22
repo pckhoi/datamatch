734e3e91ed039d7f65f87dd19fbdefdef1fb25f1
Khoi Pham <pckhoi@gmail.com>
2021-05-22 16:22:15 +0700

    export MultiIndex and Swap

--- datamatch/__init__.py
+++ datamatch/__init__.py
@@ -1,6 +1,9 @@
-from .indices import NoopIndex, ColumnsIndex
+from .indices import NoopIndex, ColumnsIndex, MultiIndex
 from .similarities import StringSimilarity, DateSimilarity, JaroWinklerSimilarity
 from .matchers import ThresholdMatcher
+from .variators import Variator, Swap
 
-__all__ = ["NoopIndex", "ColumnsIndex", "JaroWinklerSimilarity",
-           "StringSimilarity", "DateSimilarity", "ThresholdMatcher"]
+__all__ = [
+    "NoopIndex", "ColumnsIndex", "JaroWinklerSimilarity", "StringSimilarity",
+    "DateSimilarity", "ThresholdMatcher", "MultiIndex", "Variator", "Swap"
+]
