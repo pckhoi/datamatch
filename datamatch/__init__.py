c2f6ad6363c89c654741a6d0fb3fba564243d6c7
Khoi Pham <pckhoi@gmail.com>
2021-06-16 14:18:51 +0700

    Export filters

--- datamatch/__init__.py
+++ datamatch/__init__.py
@@ -2,8 +2,10 @@ from .indices import NoopIndex, ColumnsIndex, MultiIndex
 from .similarities import StringSimilarity, DateSimilarity, JaroWinklerSimilarity
 from .matchers import ThresholdMatcher
 from .variators import Variator, Swap
+from .filters import DissimilarFilter, NonOverlappingFilter
 
 __all__ = [
     "NoopIndex", "ColumnsIndex", "JaroWinklerSimilarity", "StringSimilarity",
-    "DateSimilarity", "ThresholdMatcher", "MultiIndex", "Variator", "Swap"
+    "DateSimilarity", "ThresholdMatcher", "MultiIndex", "Variator", "Swap",
+    "DissimilarFilter", "NonOverlappingFilter"
 ]
