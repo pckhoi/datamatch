1782bdac507176daa36c92c16cb90f4fe969d45a
Khoi Pham <pckhoi@gmail.com>
2021-09-01 16:27:55 +0700

    bump version

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Khoi Pham'
 author = 'Khoi Pham'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.9'
+release = '0.1.10'
 
 
 # -- General configuration ---------------------------------------------------
