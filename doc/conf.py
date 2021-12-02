e0ab9a247118974e225cd337a20647733e4c593f
Khoi Pham <pckhoi@gmail.com>
2021-12-02 08:38:08 +0700

    doc: up version

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Khoi Pham'
 author = 'Khoi Pham'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.13'
+release = '0.1.14'
 
 
 # -- General configuration ---------------------------------------------------
